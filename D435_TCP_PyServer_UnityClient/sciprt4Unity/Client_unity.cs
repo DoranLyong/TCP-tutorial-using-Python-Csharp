07609ae60a54d7442d75224e779a6280180c3a95
DoranLyong <cheeryun@gmail.com>
2020-03-04 17:44:08 +0900

    TCP_for_Unity

---
+++ D435_TCP_PyServer_UnityClient/sciprt4Unity/Client_unity.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace imageStream_client
+{ 
+
+public class Client_unity : MonoBehaviour
+{
+    IPHostEntry ipHostInfo;
+    IPAddress ipAddress; // IPv4 
+    TcpClient client_sock;
+    const int PORT = 8080;
+
+    NetworkStream stream;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("*****Unity frame started *****");
+
+        ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+        ipAddress = ipHostInfo.AddressList[1];
+        client_sock = new TcpClient(ipAddress.ToString(), PORT);
+        Debug.Log("***** Client Connected to the server *****");
+
+        stream = client_sock.GetStream();        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("**** Buffer streaming ****");
+
+        // _Client -> Server 
+        string message = "1";
+        byte[] buff = Encoding.ASCII.GetBytes(message);
+
+        stream.Write(buff, 0, buff.Length); // spend the byte stream into the Stream 
+
+
+        // _Client <- Server 
+        byte[] recvBuf = new byte[client_sock.ReceiveBufferSize]; // total receiveBuffer size 
+        int readBytes = stream.Read(recvBuf, 0, recvBuf.Length);
+
+        //Debug.Log($"total receiveBuffer length: {recvBuf.Length}");
+        //Debug.Log($"Real-read byte length: {readBytes}");
+
+
+        // _Set display image 
+        byte[] image = new byte[readBytes];
+        Buffer.BlockCopy(recvBuf, 0, image, 0, readBytes);
+
+        Viewer.instance.SetImageToDisplay(image);
+        
+
+
+
+    }
+}
+}
--- D435_TCP_PyServer_UnityClient/sciprt4Unity/Viewer.cs
+++ D435_TCP_PyServer_UnityClient/sciprt4Unity/Viewer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace HD
+namespace imageStream_client
 {
     public class Viewer : MonoBehaviour
     {
@@ -18,7 +18,8 @@ namespace HD
         #region Get Set
         public void SetImageToDisplay(byte[] image)
         {
-            instance.imageToDisplay = image;
+            DisplayImage(image);
+            //instance.imageToDisplay = image;
         }
         #endregion
 
@@ -30,13 +31,16 @@ namespace HD
             if (image.Length > 0)
             {
                 //이미지 크기 생성 이후 streaming 되는 이미지 크기에 맞게 수정 해야함
-                Texture2D texture = new Texture2D(910, 664);
+                Texture2D texture = new Texture2D(640, 480);
                 //byte형식의 데이터를 texture2D 형식으로 읽음
-                texture.LoadImage(image);
-                //이미지를 화면에 입힘(Canvas 아래 RawImage)
-                instance.rawimage.texture = texture as Texture;
-                //이미지 가로 세로 비율
-                instance.fit.aspectRatio = 910/664;
+                bool load = texture.LoadImage(image);
+                if (load)
+                {
+                    //이미지를 화면에 입힘(Canvas 아래 RawImage)
+                    instance.rawimage.texture = texture as Texture;
+                    //이미지 가로 세로 비율
+                    instance.fit.aspectRatio = 640 / 480;
+                }
             }
         }
         #endregion
@@ -49,7 +53,7 @@ namespace HD
         // Update is called once per frame
         void Update()
         {
-            DisplayImage(instance.imageToDisplay);
+            //DisplayImage(instance.imageToDisplay);
         }
     }
 }
