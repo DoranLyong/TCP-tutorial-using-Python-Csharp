e8f2f30e3a295b2ab6b70e7d772661050aed3214
DoranLyong <cheeryun@gmail.com>
2020-03-11 16:06:34 +0900

    python_unity_imageStream_ver2

---
+++ Python_Unity_imageStream(ver2)/D435_PyServer_TCP_4CS.py
@@ -0,0 +1,160 @@
+""" 
+Code author : DoranLyong 
+
+Reference : 
+* https://docs.python.org/3.7/library/socketserver.html
+* https://webnautes.tistory.com/1382
+* https://github.com/IntelRealSense/librealsense/tree/master/wrappers/python/examples
+* http://blog.cogitomethods.com/visual-analytics-using-opencv-and-realsense-camera/
+"""
+import pyrealsense2 as d435
+import socketserver
+import socket
+import cv2 
+import numpy as np 
+from queue import Queue
+from _thread import *
+
+
+# _Set queue 
+enclosure_queue = Queue() 
+
+
+# _Configures of depth and color streams 
+pipeline = d435.pipeline()
+config = d435.config()
+config.enable_stream(d435.stream.depth, 640, 480, d435.format.z16, 30)
+config.enable_stream(d435.stream.color, 640, 480, d435.format.bgr8, 30)
+
+
+
+# _ D435 process 
+def D435(queue):
+    
+    
+    print("D435 processing", end="\n ")
+    pipeline.start(config) # _Start streaming
+
+    try:
+        while True: 
+            # _Wait for a coherent pair of frames: depth and color 
+            frames = pipeline.wait_for_frames()            
+            depth_frame, color_frame = (frames.get_depth_frame(), frames.get_color_frame())
+
+            if not (depth_frame and color_frame): 
+                print("Missing frame...", end="\n")
+                continue
+
+            # _Convert <pyrealsense2 frame> to <ndarray>
+            depth_image = np.asanyarray(depth_frame.get_data()) # convert any array to <ndarray>
+            color_image = np.asanyarray(color_frame.get_data())
+
+
+            # _Apply colormap on depth image 
+            #  (image must be converted to 8-bit per pixel first)
+            
+            
+            
+            depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.05), cv2.COLORMAP_BONE)
+            #depth_colormap  = cv2.bitwise_not(depth_colormap ) # reverse image
+
+            #print("Depth map shape = ", depth_colormap.shape)   
+
+
+            # _Encoding 
+            target_frame = depth_colormap
+            
+            #print(target_frame.shape)
+            
+
+            encode_param = [int(cv2.IMWRITE_JPEG_QUALITY),80]  # 0 ~ 100 quality 
+            #encode_param = [cv2.IMWRITE_PNG_COMPRESSION,0] # 0 ~ 9 Compressiong rate 
+            #encode_param = [int(cv2.IMWRITE_WEBP_QUALITY),95]  # 0 ~ 100 quality 
+
+
+
+            result, imgencode = cv2.imencode('.jpg', target_frame, encode_param)  # Encode numpy into '.jpg'
+            data = np.array(imgencode)
+
+            stringData = data.tostring()   # Convert numpy to string
+            #print("byte Length: ", len(stringData))
+            queue.put(stringData)          # Put the encode in the queue stack
+
+
+            # __ Image show             
+            images = np.hstack((color_image, depth_colormap)) # stack both images horizontally             
+            
+            cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+            cv2.imshow('RealSense', images)
+            cv2.waitKey(1)        
+
+        
+
+    finally: 
+        cv2.destroyAllWindows()
+
+        # _Stop streaming 
+        pipeline.stop()
+
+    
+
+
+
+
+
+class MyTCPHandler(socketserver.BaseRequestHandler):
+
+    queue  = enclosure_queue 
+    stringData = str()
+
+    def handle(self):
+       
+        # 'self.request' is the TCP socket connected to the client     
+        print("A client connected by: ", self.client_address[0], ":", self.client_address[1] )
+
+
+        while True:
+            try:
+                # _server <- client 
+                self.data = self.request.recv(1024).strip()   # 1024 byte for header 
+                #print("Received from client: ", self.data)
+
+                if not self.data: 
+                    print("The client disconnected by: ", self.client_address[0], ":", self.client_address[1] )     
+                    break                
+
+                # _Get data from Queue stack 
+                MyTCPHandler.stringData = MyTCPHandler.queue.get()     
+
+                # _server -> client 
+                #print(str(len(MyTCPHandler.stringData)).ljust(16).encode())  # <str>.ljust(16) and encode <str> to <bytearray>
+                
+                ###self.request.sendall(str(len(MyTCPHandler.stringData)).ljust(16).encode())  # <- Make this line ignored when you connect with C# client. 
+                self.request.sendall(MyTCPHandler.stringData)  
+
+                
+                #self.request.sendall(len(MyTCPHandler.stringData).to_bytes(1024, byteorder= "big"))
+                #self.request.sendall(MyTCPHandler.stringData)             
+
+                
+            except ConnectionResetError as e: 
+                print("The client disconnected by: ", self.client_address[0], ":", self.client_address[1] )     
+                break
+
+
+if __name__ == "__main__":
+
+    # _Webcam process is loaded onto subthread
+    start_new_thread(D435, (enclosure_queue,))  
+    
+    # _Server on
+    HOST, PORT = socket.gethostname(), 8080 
+    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:    
+        
+        print("****** Server started ****** ", end="\n \n")     
+        
+        try: 
+            server.serve_forever()
+        
+        except KeyboardInterrupt as e:
+            print("******  Server closed ****** ", end="\n \n" )  
---
+++ Python_Unity_imageStream(ver2)/README.md
@@ -0,0 +1,29 @@
+# Usage sample
+* Start \'D435_PyServer_TCP.py\' first  <br/>
+* This example is tested using the [Intel D435](https://www.intelrealsense.com/depth-camera-d435/) for depth image streaming.<br/>
+    * if you want more example codes for Intel cameras using python, go to the github link below. 
+
+<br/>
+
+This project is fixed from the first version. <br/>
+* python server code had been fixed for TCP client in C#. 
+
+You can control the spent image quality in python server code. <br/>
+*  ```encode_param = [int(cv2.IMWRITE_JPEG_QUALITY),80]```  
+* ``` 80 → 100 ``` makes images in 100% quality, but it can overflow the buffer size.
+
+## Test 
+It's not perfect yet. <br/>
+
+<img src="./Python_unity.gif" width=650>
+
+
+***
+
+### Reference 
+* [Sample Code for Intel RealSense Python Wrapper, Github](https://github.com/IntelRealSense/librealsense/tree/master/wrappers/python/examples)
+* [Visual Analytics using OpenCV and RealSense Camera](http://blog.cogitomethods.com/visual-analytics-using-opencv-and-realsense-camera/)
+* [How to save a texture2d into a PNG?, unity forums](https://answers.unity.com/questions/1331297/how-to-save-a-texture2d-into-a-png.html)
+* [While loop not working?, unity forums](https://forum.unity.com/threads/while-loop-not-working.429208/)
+* [TCP socket communication for image streaming and Texture2D error, stack overflow](https://stackoverflow.com/questions/60576364/tcp-socket-communication-for-image-streaming-and-texture2d-error)
+
